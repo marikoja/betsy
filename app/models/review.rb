9fca14e301a690d0f3eb1d0f83accfe2eee17c84
marikoja <Marikoanderson@gmail.com>
2018-04-19 09:50:27 -0700

    refactored validations for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,9 @@ class Review < ApplicationRecord
 
   belongs_to :products
 
-  validates :rating, pressence: true
-  validates :rating, numericality: { only_integer: true }
-  validates :rating, inclusion: { in: 1..5}
+  validates :rating, {
+    pressence: true,
+    numericality: { only_integer: true },
+    inclusion: { in: 1..5}
+  }
 end
