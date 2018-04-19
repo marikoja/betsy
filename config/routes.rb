0a4bea54ae5c1bd925347e6946793048ed01330b
marikoja <Marikoanderson@gmail.com>
2018-04-19 15:55:24 -0700

    resloved merge conflicts in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
+  root 'products#index'
 
   get "/auth/:provider/callback", to: "sessions#create", as: "auth_callback"
   get '/auth/github', as: 'github_login'
 
-<<<<<<< HEAD
   # resources :sessions
   #
   # resources :products
@@ -32,21 +21,5 @@ Rails.application.routes.draw do
   end
 
   # resources :order_items
-=======
-  root 'products#index'
 
-  resources :sessions
-
-  resources :products
-
-  resources :categories
-
-  resources :carts
-
-  resources :reviews
-
-  resources :users
-
-  resources :order_items
->>>>>>> a05701ef1fbecdcf3166ea3ca454ab3b3286871d
 end
