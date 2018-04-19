77a746e90f812f0e58f1a7418c74bf31e1128ad0
Abiaina <abinnet.a@gmail.com>
2018-04-19 14:23:16 -0700

    created fixtures that all relate to each other, follow fruit theme. missing product categories

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -4,6 +4,10 @@ describe OrderItem do
   let(:order_item) { OrderItem.new }
 
   it "must be valid" do
+    @test = order_item
+    @test.product =
+    @test.order =
+    @test.save
     value(order_item).must_be :valid?
   end
 end
