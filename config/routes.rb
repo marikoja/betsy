5b524e72acf2d2cc34262b6b03b5497f682471a1
Mary Lamkin <mmlamkin@gmail.com>
2018-04-19 08:54:42 -0700

    adds controller methods for products and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  get 'categories/new'
+
+  get 'categories/create'
+
+  get 'categories/edit'
+
+  get 'categories/update'
+
+  get 'categories/show'
+
+  get 'categories/destroy'
+
+  get 'products/index'
+
+  get 'products/new'
+
+  get 'products/create'
+
+  get 'products/edit'
+
+  get 'products/update'
+
+  get 'products/show'
+
+  get 'products/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
