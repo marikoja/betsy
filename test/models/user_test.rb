ab514b6587c6bd03b4129518bf04f5f8e21f9c80
Abiaina <abinnet.a@gmail.com>
2018-04-19 23:26:10 -0700

    user validation test completed, failed and passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  let(:user) { User.new name: "Bey", email: "bey@yonce.com", uid: 2231, provider: "yonce"}
 
   it "must be valid" do
+    user.save
     value(user).must_be :valid?
+  end
+
+  it "is invalid when missing a name" do
+    user.name = nil
+    user.save
+    user.valid?.must_equal false
+  end
+
+  it "is invalid when given a non-unique name" do
+    user.name = "Bey"
+    user.save
+    dup_user_name = User.new
+    dup_user_name.name = "Bey"
+    dup_user_name.save
+    dup_user_name.valid?.must_equal false
   end
 end
