c7e294c8b7a7313d20d0011546c544de878d6f00
Phoebe <phoebe.manning8@gmail.com>
2018-04-20 14:46:24 -0700

    comments out user logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,25 +5,40 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
-  def self.avg_rating(user)
-    sum = 0
-    user_products = user.products
-    product_count = user.products.count
+  # def self.avg_rating(user)
+  #   sum = 0
+  #   user_products = user.products
+  #   product_count = user.products.count
+  #
+  #   if product_count == 0
+  #     return sum
+  #   else
+  #     user_products.each do |product|
+  #       # iterate through all of the product reviews
+  #       reviews = product.reviews
+  #
+  #       reviews.each do |review|
+  #         sum += review.rating
+  #       end
+  #     end
+  #
+  #     return (sum/product_count).to_f
+  #   end
+  # end
 
-    if product_count == 0
-      return sum
-    else
-      user_products.each do |product|
-        # iterate through all of the product reviews
-        reviews = product.reviews
+  # def self.highest_rated_product(user)
+  #   product_count = user.products.count
+  #   if product_count == 0
+  #     return
+  #   else
+  #     max = user.products.first
+  #     user_products = user.products
+  #     user_products.each do |product|
+  #       # if product.re
+  #       #
+  #       # end
+  #     end
+  #   end
 
-        reviews.each do |review|
-          sum += review.rating
-        end
-      end
-
-      return (sum/product_count).to_f
-    end
-
-  end
+  # end
 end
