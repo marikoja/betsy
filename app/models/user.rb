0573d47bb32a13420c1d2a8f76ff4fd80d299fea
Phoebe <phoebe.manning8@gmail.com>
2018-04-20 12:29:56 -0700

    adds average rating logic to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,26 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  def self.avg_rating(user)
+    sum = 0
+    user_products = user.products
+    product_count = user.products.count
+
+    if product_count == 0
+      return sum
+    else
+      user_products.each do |product|
+        # iterate through all of the product reviews
+        reviews = product.reviews
+
+        reviews.each do |review|
+          sum += review.rating
+        end
+      end
+
+      return (sum/product_count).to_f
+    end
+
+  end
 end
