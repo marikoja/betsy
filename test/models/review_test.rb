020faef07bb7f32e0be024f57c85ab6380ecf48c
Mary Lamkin <mmlamkin@gmail.com>
2018-04-21 17:02:24 -0700

    user model tests passing

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -4,36 +4,31 @@ describe Review do
   let(:review) { Review.create rating: 4, text_review: "very fresh!" }
 
   it "must be valid" do
-    product = Product.create(name: "green apple", price: 2)
-    # why is this not updating into the review product_id?
-    review.product_id = product.id
-    review.save
+    review = reviews(:rating1)
     value(review).must_be :valid?
   end
 
   it "is invalid when missing a rating" do
     review.rating = nil
-    review.save
     review.valid?.must_equal false
   end
 
   it "is invalid when a rating greater than 5" do
     review.rating = 6
-    review.save
     review.valid?.must_equal false
   end
 
   it "is invalid when a rating less than 1" do
     review.rating = 0
-    review.save
     review.valid?.must_equal false
   end
 
+  it "must have a product" do
+    review.valid?.must_equal false
 
+    review.product = products(:cherries)
 
-  describe "relations" do
-    it "belongs to products" do
-
-    end
+    review.valid?.must_equal true
   end
+
 end
