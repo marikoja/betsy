42d3b8fbffcece0bb5ddb3ed7c45484c3c64f347
marikoja <Marikoanderson@gmail.com>
2018-04-21 23:29:31 -0700

    Changed schema products_categories to categroies_products, relations working now

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_and_belongs_to_many :categories, join_table: :products_categories
+  has_and_belongs_to_many :categories, join_table: :categories_products
   belongs_to :user
   has_many :reviews
 
