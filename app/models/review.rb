80a6abdbb107129971783d47ac0fe96a40dc326b
Abiaina <abinnet.a@gmail.com>
2018-04-21 17:48:45 -0700

    users can review products, products reviews are listed on product detail page

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,6 +8,15 @@ class Review < ApplicationRecord
     inclusion: { in: 1..5}
   }
 
+  # validate that the product --> product.user_id --> doesn't match the login session id of user
+  # def no_self_reviews
+  #   product = Product.find(session[:product_id])
+  #   user = User.find(product.user_id)
+  #
+  #   if user.id == sessions[:user_id]
+  #
+  # end
+
   # should this be in product or in reviews model?
   # def self.average_rating(reviews)
   #   total = 0
