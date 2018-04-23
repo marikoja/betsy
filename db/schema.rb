b8d0d522f2692b2e00e5e0bed2c46c849a1c7114
Abiaina <abinnet.a@gmail.com>
2018-04-23 14:28:10 -0700

    removed categories products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422051721) do
+ActiveRecord::Schema.define(version: 20180423212509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,13 +64,6 @@ ActiveRecord::Schema.define(version: 20180422051721) do
     t.string "image"
     t.integer "user_id"
     t.integer "quantity"
-  end
-
-  create_table "products_categories", force: :cascade do |t|
-    t.bigint "product_id"
-    t.bigint "category_id"
-    t.index ["category_id"], name: "index_products_categories_on_category_id"
-    t.index ["product_id"], name: "index_products_categories_on_product_id"
   end
 
   create_table "reviews", force: :cascade do |t|
