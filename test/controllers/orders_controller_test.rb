5fce8e837515ae5b544c4d183cf3b95faddccc5c
Abiaina <abinnet.a@gmail.com>
2018-04-24 13:39:56 -0700

    added state to orders _form

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -7,6 +7,11 @@ describe OrdersController do
   end
 
   describe "creating new order" do
+    before do
+      test_user = User.new
+      test_user.name = users(:beyonce).name
+      test_user.save
+    end
     it "should get new" do
       test_user = User.new
       test_user.name = users(:beyonce).name
@@ -14,10 +19,10 @@ describe OrdersController do
       get new_order_path(test_user.id)
       value(response).must_be :success?
     end
-    # it "should create an order with valid inputs" do
-    #   test = Order.create(orders :cart1)
-    #   test.valid?.must_be true
-    # end
+    it "should create an order with valid inputs" do
+      test = Order.create(orders :cart1)
+      test.valid?.must_be true
+    end
   end
 
 end
