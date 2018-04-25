c63d0634f503b3d24416c03ebe7622c18431a8ef
Abiaina <abinnet.a@gmail.com>
2018-04-24 21:23:49 -0700

    created mock OAuth in test helper and user controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,68 +2,68 @@ require "test_helper"
 
 describe UsersController do
 
-  describe "index" do
-    it "succeeds when there are users" do
-
-      get users_path
-      must_respond_with :success
-    end
-
-    it "succeeds when there are no users" do
-
-      users = User.all
-      users.each do |user|
-        user.products.destroy
-        user.destroy
-      end
-      get users_path
-      must_respond_with :success
-    end
-
-  end
-
-  describe "create" do
-    it "succeeds with an existing username and redirects to root_path" do
-    
-
-      must_redirect_to root_path
-
-    end
-
-    it "succeeds with new valid username and redirects to root path" do
-      new_user = User.new(
-        provider: 'githib',
-        uid: 999,
-        username: 'test user',
-        email: 'test@test.com'
-      )
-
-      proc {
-        perform_login(new_user)
-      }.must_change 'User.count', 1
-
-
-      must_respond_with :redirect
-      must_redirect_to root_path
-    end
-
-    it "renders bad_request with invalid username" do
-      skip
-      new_user = User.new(
-        provider: 'githib',
-        uid: 999,
-        username:'',
-        email: 'test@test.com'
-      )
-
-      proc {
-        perform_login(new_user)
-      }.must_change 'User.count', 0
-
-
-      must_respond_with :bad_request
-    end
-  end
+  # describe "index" do
+  #   it "succeeds when there are users" do
+  #
+  #     get users_path
+  #     must_respond_with :success
+  #   end
+  #
+  #   it "succeeds when there are no users" do
+  #
+  #     users = User.all
+  #     users.each do |user|
+  #       user.products.destroy
+  #       user.destroy
+  #     end
+  #     get users_path
+  #     must_respond_with :success
+  #   end
+  #
+  # end
+  #
+  # describe "create" do
+  #   it "succeeds with an existing username and redirects to root_path" do
+  #
+  #
+  #     must_redirect_to root_path
+  #
+  #   end
+  #
+  #   it "succeeds with new valid username and redirects to root path" do
+  #     new_user = User.new(
+  #       provider: 'githib',
+  #       uid: 999,
+  #       username: 'test user',
+  #       email: 'test@test.com'
+  #     )
+  #
+  #     proc {
+  #       perform_login(new_user)
+  #     }.must_change 'User.count', 1
+  #
+  #
+  #     must_respond_with :redirect
+  #     must_redirect_to root_path
+  #   end
+  #
+  #   it "renders bad_request with invalid username" do
+  #     skip
+  #     new_user = User.new(
+  #       provider: 'githib',
+  #       uid: 999,
+  #       username:'',
+  #       email: 'test@test.com'
+  #     )
+  #
+  #     proc {
+  #       perform_login(new_user)
+  #     }.must_change 'User.count', 0
+  #
+  #
+  #     must_respond_with :bad_request
+  #   end
+  # end
   # it "should get index" do
   #   get users_index_url
   #   value(response).must_be :success?
@@ -93,5 +93,43 @@ describe UsersController do
   #   get users_update_url
   #   value(response).must_be :success?
   # end
+
+  ###### The above controller tests are great. I just added the OAuth tests below: #######
+
+  describe "auth_callback" do
+   it "logs in an existing user and redirects to the root route" do
+     # Count the users, to make sure we're not (for example) creating
+     # a new user every time we get a login request
+     start_count = User.count
+
+     # Get a user from the fixtures
+     user = users(:beyonce)
+
+     # Tell OmniAuth to use this user's info when it sees
+     # an auth callback from github
+     OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+     # Send a login request for that user
+     # Note that we're using the named path for the callback, as defined
+     # in the `as:` clause in `config/routes.rb`
+     get auth_callback_path(:github)
+
+     must_redirect_to root_path
+
+     # Since we can read the session, check that the user ID was set as expected
+     session[:user_id].must_equal user.id
+
+     # Should *not* have created a new user
+     User.count.must_equal start_count
+   end
+
+   it "creates an account for a new user and redirects to the root route" do
+   end
+
+   it "redirects to the login route if given invalid user data" do
+   end
+ end
+
+
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,5 +25,23 @@ Minitest::Reporters.use!(
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  # Add more helper methods to be used by all tests here...
+
+  def setup
+    # Once you have enabled test mode, all requests
+    # to OmniAuth will be short circuited to use the mock authentication hash.
+    # A request to /auth/provider will redirect immediately to /auth/provider/callback.
+    OmniAuth.config.test_mode = true
+  end
+
+  def mock_auth_hash(user)
+   return {
+     provider: user.provider,
+     uid: user.uid,
+     info: {
+       email: user.email,
+       nickname: user.name
+     }
+   }
+ end
+
 end
